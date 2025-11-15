0b8043baed442351eda70878cf66695e71dcd144
Carlosgalvis77 <cgalvisgo@unal.edu.co>
2025-11-15 13:45:23 -0500

    Update controlInventarioBodega.jsx

--- Proyecto/src/pantallasJefe/controlInventarioBodega.jsx
+++ Proyecto/src/pantallasJefe/controlInventarioBodega.jsx
@@ -9,6 +9,36 @@ export default function ControlInventarioBodega({volverAlInicio, volverLoginJefe
 
                 </label>
                 <br/><br/>
+
+                <div>
+                    <table style={{ border: "1px solid white", borderCollapse: "collapse" }}>
+                        <tr>
+                        <th style={{ border: "1px solid white", padding: "8px" }}>Producto</th>
+                        <th style={{ border: "1px solid white", padding: "8px" }}>Cantidad</th>
+                        </tr>
+
+                        <tr>
+                        <td style={{ border: "1px solid white", padding: "8px" }}>Vasos</td>
+                        <td style={{ border: "1px solid white", padding: "8px" }}>120</td>
+                        </tr>
+
+                        <tr>
+                        <td style={{ border: "1px solid white", padding: "8px" }}>Cocacola</td>
+                        <td style={{ border: "1px solid white", padding: "8px" }}>120</td>
+                        </tr>
+
+                        <tr>
+                        <td style={{ border: "1px solid white", padding: "8px" }}>Platos</td>
+                        <td style={{ border: "1px solid white", padding: "8px" }}>120</td>
+                        </tr>
+
+                        <tr>
+                        <td style={{ border: "1px solid white", padding: "8px" }}>Salsa</td>
+                        <td style={{ border: "1px solid white", padding: "8px" }}>120</td>
+                        </tr>
+                    </table>
+                    </div>
+
                 <button
                     type="button"
                     onClick={volverLoginJefe}> 
